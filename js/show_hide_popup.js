d4c5af83d90b1eb57896ab844368c95985a3fe17
Adriana Gabriela Munoz Cruz <contacto@gabs.com.mx>
2023-03-31 07:51:29 -0600

    Banners

---
+++ js/show_hide_popup.js
@@ -0,0 +1,256 @@
+﻿
+function show_hide_popup() {
+	//$(".content_popup").hide().eq(0).show(); 
+	$("#show_popup_01").click(function() {
+        $("#screen_full_transparent").fadeIn(200);
+		$(".content_popup").hide();
+        $("#content_popup_01").show();
+    }); 
+	
+		$("#show_popup_02").click(function() {
+        $("#screen_full_transparent").fadeIn(200);
+		$(".content_popup").hide();
+        $("#content_popup_02").show();
+    }); 
+
+	$("#show_popup_03").click(function() {
+        $("#screen_full_transparent").fadeIn(200);
+		$(".content_popup").hide();
+        $("#content_popup_03").show();
+    }); 
+
+	$("#show_popup_04").click(function() {
+        $("#screen_full_transparent").fadeIn(200);
+		$(".content_popup").hide();
+        $("#content_popup_04").show();
+    });
+	
+	$("#show_popup_05").click(function() {
+        $("#screen_full_transparent").fadeIn(200);
+		$(".content_popup").hide();
+        $("#content_popup_05").show();
+    });
+
+	$("#show_popup_06").click(function() {
+        $("#screen_full_transparent").fadeIn(200);
+		$(".content_popup").hide();
+        $("#content_popup_06").show();
+    });
+
+	$("#show_popup_07").click(function() {
+        $("#screen_full_transparent").fadeIn(200);
+		$(".content_popup").hide();
+        $("#content_popup_07").show();
+    });
+
+    $("#show_add_01").click(function() {
+        $("#screen_full_transparent").fadeIn(200);
+		$(".content_popup").hide();
+        $("#content_add_01").show();
+    });
+
+	$("#show_add_02").click(function() {
+        $("#screen_full_transparent").fadeIn(200);
+		$(".content_popup").hide();
+        $("#content_add_02").show();
+    });
+
+	$("#show_add_03").click(function() {
+        $("#screen_full_transparent").fadeIn(200);
+		$(".content_popup").hide();
+        $("#content_add_03").show();
+    });
+
+	$("#show_add_04").click(function() {
+        $("#screen_full_transparent").fadeIn(200);
+		$(".content_popup").hide();
+        $("#content_add_04").show();
+    });
+
+	
+	$("#show_popup_1").click(function() {
+        $("#screen_full_transparent").fadeIn(200);
+		$(".content_popup").hide();
+        $("#content_popup_1").show();
+    }); 
+	
+	$("#show_popup_2").click(function() {
+        $("#screen_full_transparent").fadeIn(200);
+		$(".content_popup").hide();
+        $("#content_popup_2").show();
+		//$("#content_popup_2").css("display", "block");
+    }); 
+	
+	$("#show_popup_3").click(function() {
+        $("#screen_full_transparent").fadeIn(200);
+		$(".content_popup").hide();
+        $("#content_popup_3").show();
+		//$("#content_popup_2").css("display", "block");
+    }); 
+	
+	$("#show_popup_4").click(function() {
+        $("#screen_full_transparent").fadeIn(200);
+		$(".content_popup").hide();
+        $("#content_popup_4").show();
+		//$("#content_popup_2").css("display", "block");
+    }); 
+	
+	$("#show_popup_5").click(function() {
+        $("#screen_full_transparent").fadeIn(200);
+		$(".content_popup").hide();
+        $("#content_popup_5").show();
+		//$("#content_popup_2").css("display", "block");
+    }); 
+	
+	$("#show_popup_6").click(function() {
+        $("#screen_full_transparent").fadeIn(200);
+		$(".content_popup").hide();
+        $("#content_popup_6").show();
+		//$("#content_popup_2").css("display", "block");
+    }); 
+	
+	$("#show_popup_7").click(function() {
+        $("#screen_full_transparent").fadeIn(200);
+		$(".content_popup").hide();
+        $("#content_popup_7").show();
+		//$("#content_popup_2").css("display", "block");
+    }); 
+	
+	
+	$("#show_popup_8").click(function() {
+        $("#screen_full_transparent").fadeIn(200);
+		$(".content_popup").hide();
+        $("#content_popup_8").show();
+		//$("#content_popup_2").css("display", "block");
+    });
+	
+	$("#show_popup_9").click(function() {
+        $("#screen_full_transparent").fadeIn(200);
+		$(".content_popup").hide();
+        $("#content_popup_9").show();
+		//$("#content_popup_2").css("display", "block");
+    });
+	
+	$("#show_popup_10").click(function() {
+        $("#screen_full_transparent").fadeIn(200);
+		$(".content_popup").hide();
+        $("#content_popup_10").show();
+		//$("#content_popup_2").css("display", "block");
+    });
+//Animación
+	$("#show_ani_1").click(function() {
+        $("#screen_full_transparent").fadeIn(200);
+		$(".content_popup").hide();
+        $("#content_ani_1").show();
+		//$("#content_popup_1").css("display", "block");
+    }); 
+	
+	$("#show_ani_2").click(function() {
+        $("#screen_full_transparent").fadeIn(200);
+		$(".content_popup").hide();
+        $("#content_ani_2").show();
+		//$("#content_popup_2").css("display", "block");
+    }); 
+	
+	$("#show_ani_3").click(function() {
+        $("#screen_full_transparent").fadeIn(200);
+		$(".content_popup").hide();
+        $("#content_ani_3").show();
+		//$("#content_popup_2").css("display", "block");
+    }); 
+	
+	$("#show_ani_4").click(function() {
+        $("#screen_full_transparent").fadeIn(200);
+		$(".content_popup").hide();
+        $("#content_ani_4").show();
+		//$("#content_popup_2").css("display", "block");
+    }); 
+	
+	$("#show_ani_5").click(function() {
+        $("#screen_full_transparent").fadeIn(200);
+		$(".content_popup").hide();
+        $("#content_ani_5").show();
+		//$("#content_popup_2").css("display", "block");
+    }); 
+	
+	
+	$("#show_ani_6").click(function() {
+        $("#screen_full_transparent").fadeIn(200);
+		$(".content_popup").hide();
+        $("#content_ani_6").show();
+		//$("#content_popup_2").css("display", "block");
+    }); 
+	
+	$("#show_ani_7").click(function() {
+        $("#screen_full_transparent").fadeIn(200);
+		$(".content_popup").hide();
+        $("#content_ani_7").show();
+		//$("#content_popup_2").css("display", "block");
+    }); 
+	
+	$("#show_ani_8").click(function() {
+        $("#screen_full_transparent").fadeIn(200);
+		$(".content_popup").hide();
+        $("#content_ani_8").show();
+		//$("#content_popup_2").css("display", "block");
+    }); 
+	
+		$("#show_ani_9").click(function() {
+        $("#screen_full_transparent").fadeIn(200);
+		$(".content_popup").hide();
+        $("#content_ani_9").show();
+		//$("#content_popup_2").css("display", "block");
+    });
+
+//video 
+	$("#show_video_1").click(function() {
+        $("#screen_full_transparent").fadeIn(200);
+		$(".content_popup").hide();
+        $("#content_video_1").show();
+		//$("#content_popup_1").css("display", "block");
+    }); 
+	
+	$("#show_video_2").click(function() {
+        $("#screen_full_transparent").fadeIn(200);
+		$(".content_popup").hide();
+        $("#content_video_2").show();
+		//$("#content_popup_2").css("display", "block");
+    }); 
+	
+	$("#show_video_3").click(function() {
+        $("#screen_full_transparent").fadeIn(200);
+		$(".content_popup").hide();
+        $("#content_video_3").show();
+		//$("#content_popup_2").css("display", "block");
+    }); 
+	
+	$("#show_video_4").click(function() {
+        $("#screen_full_transparent").fadeIn(200);
+		$(".content_popup").hide();
+        $("#content_video_4").show();
+		//$("#content_popup_2").css("display", "block");
+    }); 
+
+
+    $(".close_popup").click(function() {
+        $("#screen_full_transparent").fadeOut(200);
+    });
+}
+
+// JavaScript Document
+
+function fn_change_image_jquery(){	
+	$("#img_image").mouseover(function() {
+		$("#img_image").attr('src',"img/foco_over.png");			
+	});
+	$("#img_image").mouseout(function() {
+		$("#img_image").attr('src',"img/foco_roll.png");			
+	});
+	
+	$("#img_repetir").mouseover(function() {
+		$("#img_repetir").attr('src',"../img/btn_repetir_over.png");			
+	});
+	$("#img_repetir").mouseout(function() {
+		$("#img_repetir").attr('src',"../img/repertir_roll.png");			
+	});
+}
